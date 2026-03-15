343094577036039c83bfa1c1a43a0cb68b2c6d60
Bauer <bauerlucks@gmail.com>
2026-03-15 07:34:01 -0300

    Update webui-user.bat
    teste

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PYTHON=
+set PYTHON=C:\Users\lnpot\AppData\Local\Programs\Python\Python310\python.exe
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
-
+set COMMANDLINE_ARGS=--lowvram --xformers
+set STABLE_DIFFUSION_REPO=https://github.com/Stability-AI/stablediffusion
 call webui.bat
